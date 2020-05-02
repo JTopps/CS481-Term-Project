cf173e3061315fb99cfecc4d507068945ecd4943
aykalo <38357009+aykalo@users.noreply.github.com>
2020-05-01 18:20:03 -0600

    Update StockQuery.sol

--- StockQuery.sol
+++ StockQuery.sol
@@ -3,30 +3,38 @@ pragma solidity >= 0.5.0 < 0.6.0;
 import "github.com/provable-things/ethereum-api/provableAPI.sol";
 
 contract StockPrice is usingProvable {
-
+    
     string public stockResult;
-
+    string private api;
+    string private query;
+    
     event LogNewProvableQuery(string description);
     event LogNewStock(string stock);
-
+    
     constructor() public
     {
-        update(); // First check at contract creation...
+        //update(string); // First check at contract creation...
     }
-
-    function __callback( bytes32 _myid, string memory _result) public
+    
+    function concatenate(string memory a, string memory b) private pure returns(string memory)
+    {
+        return string(abi.encodePacked(a,b));
+    }
+    
+    function __callback(bytes32 _myid, string memory _result) public
     {
         require(msg.sender == provable_cbAddress());
         stockResult = _result;
         emit LogNewStock(stockResult);
-        // Do something with the temperature measure...
     }
-
-
-    function update() public payable
+    
+    
+    function update(string memory _symbol) public payable
     {
         emit LogNewProvableQuery("Provable query was sent, standing by for the answer...");
-        provable_query("URL", "json(https://financialmodelingprep.com/api/v3/company/profile/AAPL).profile.price");
+        api = concatenate("json(https://financialmodelingprep.com/api/v3/company/profile/", _symbol);
+        query = concatenate(api, ").profile.price");
+        provable_query("URL", query); 
     }
-
+    
 }
